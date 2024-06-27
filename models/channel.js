759d5f5436995cf869875367d32e43ed95f5c016
llihwerd <104748400+llihwerd@users.noreply.github.com>
2024-06-26 22:42:55 -0500

    add channel.js and fill out schema

---
+++ models/channel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const channelSchema = new mongoose.Schema({
+  channelId: { type: String, required: true, unique: true },
+  name: String,
+  description: String,
+  thumbnailUrl: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Channel = mongoose.model('Channel', channelSchema);
+
+export default Channel;
