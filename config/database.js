e5d3c2207f7e569ee258779eb49165c31640b2b6
llihwerd <104748400+llihwerd@users.noreply.github.com>
2024-06-27 23:06:12 -0500

    continue to add troubleshooting console logs to figure out why testFetchChannelData will not work before timing out

--- config/database.js
+++ config/database.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose'
 
 const db = mongoose.connection
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}).then(() => {
+  console.log("Successfully connected to MongoDB.")
+}).catch((error) => {
+  console.error("Connection error", error)
+})
 
 db.on('connected', function () {
   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
